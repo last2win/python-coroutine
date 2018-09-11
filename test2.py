24dc6053398a0130dc0a56cdb4c244cb2d75a76b
zhang0peter <zsd780885029@gmail.com>
2018-09-11 15:44:00 +0800

    2018-9-11

--- test2.py
+++ test2.py
@@ -1,30 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 31 16:58:51 2018
+
+@author: peter
+"""
+
 import time
 import requests
 import asyncio
-import concurrent.futures
-total = 100
-thread = 4
+total = 3
+head = {"User-Agent":"Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36"}
 
 
 async def request():
-    with concurrent.futures.ThreadPoolExecutor(thread) as executor:
-        loop = asyncio.get_event_loop()
-        futures = [
-            loop.run_in_executor(
-                executor,
-                requests.get,
-                'http://127.0.0.1:5000'
-            )
-            for i in range(0, total)
-        ]
-        for response in await asyncio.gather(*futures):
-            pass
+    url = 'http://www.baidu.com/s?ie=UTF-8&wd=python'
+    future = loop.run_in_executor(
+        None, requests.get, url,head)
+    response = await future
+    response.encoding = response.apparent_encoding
+    demo = response.text
+    print(demo)
 
 
 if __name__ == '__main__':
     time0 = time.time()
+    tasks = [asyncio.ensure_future(request()) for i in range(0, total)]
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(request())
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
     time1 = time.time()
     print("爬取{0}个网页 ，总花费时间:{1:.2f}s".format(
         total, time1-time0), end="")
